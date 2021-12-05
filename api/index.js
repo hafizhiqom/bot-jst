56cdfd14174ece2011ea47df65cecbc1976f1868
hafizhiqom <95557683+hafizhiqom@users.noreply.github.com>
2021-12-05 14:17:23 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '5068714499:AAGw6SWSL_Rlar7MdW23DRt9KKDTzqqD4fM'
 const bot = new TelegramBot(token, {polling: true});
 
 
